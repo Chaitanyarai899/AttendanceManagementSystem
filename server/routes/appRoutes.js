8b67cc4cd69eac21980a4e8d9adc7dcaad435c6b
Aditya <adityasinghboss1234@gmail.com>
2023-01-01 18:10:38 +0530

    appRoutes and webRoutes changed

--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -20,23 +20,31 @@ connection.connect(function (err) {
 });
 
 
-router.post("/api/employee", (req, res)=>{    
+router.post("/api/login", (req, res)=>{  
+    const id = req.body.id;
+    const password = req.body.password; 
+    
     connection.query(`SELECT name, designation, date_of_joining FROM employee WHERE instructor_id = ? AND password = ?`, 
-    [req.body.id, req.body.password],
+    [id, password],
     function(error, result){
         if(error){
             throw error;
         }else{
-            res.send(result);         
+            if(result[0] == null){
+                res.status(400).send({message: "User doesn't exist"})
+            }else{
+                res.send({message: "Successfully logedin", result: result})
+            }
         }
     }
     )
 });
 
 router.post("/api/classes", (req, res)=>{
-    
-    connection.query(`SELECT batch_id, subject_name, subject_code FROM edcSubjectAllocation WHERE instructor_code = ?`,
-    [req.body.id],
+    const id = req.body.id;
+
+    connection.query(`SELECT batch_id, subject_name, subject_code, type FROM subject_allocation WHERE instructor_id = ?`,
+    [id],
     function(error, result){
         if(error){
             throw error;
