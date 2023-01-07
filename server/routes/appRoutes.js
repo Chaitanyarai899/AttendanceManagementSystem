0b8a2ce47ccec8838673b5fadc071c71d4ce5e7f
Aditya <adityasinghboss1234@gmail.com>
2023-01-07 15:37:08 +0530

    Error handling in app api's
    Signed-off-by: Aditya <adityasinghboss1234@gmail.com>

--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -24,34 +24,55 @@ router.post("/api/login", (req, res)=>{
     const id = req.body.id;
     const password = req.body.password; 
     
-    connection.query(`SELECT name, designation, date_of_joining FROM employee WHERE instructor_id = ? AND password = ?`, 
-    [id, password],
-    function(error, result){
-        if(error){
-            throw error;
-        }else{
-            if(result[0] == null){
-                res.status(400).send({message: "User doesn't exist"})
+    try{
+        connection.query(`SELECT name FROM employee where instructor_id = ?`,
+        [id], 
+        function(err, result){
+            if(err){
+                throw err;
+            }else if(result[0] == null){
+                res.status(400).send({message: "User doesn't exist"});
             }else{
-                res.send({message: "Successfully logedin", result: result})
+                connection.query(`SELECT name, designation, date_of_joining FROM employee WHERE instructor_id = ? AND password = ?`, 
+                [id, password],
+                function(error, result){
+                    if(error){
+                        throw error;
+                    }else{
+                        if(result[0] == null){
+                            res.status(400).send({message: "Wrong user Id or password"})
+                        }else{
+                            res.send({message: "Successfully logedin", result: result})
+                        }
+                    }
+                }
+                )
             }
-        }
+            
+        })
+        
+    }catch{
+        res.status(500).send({message: "Internal server error"});
     }
-    )
 });
 
 router.post("/api/classes", (req, res)=>{
     const id = req.body.id;
 
-    connection.query(`SELECT batch_id, subject_name, subject_code, type FROM subject_allocation WHERE instructor_id = ?`,
-    [id],
-    function(error, result){
-        if(error){
-            throw error;
-        }else{
-            res.send(result);
-        }
-    })
+    try{
+        connection.query(`SELECT batch_id, subject_name, subject_code, type FROM subject_allocation WHERE instructor_id = ?`,
+        [id],
+        function(error, result){
+            if(error){
+                throw error;
+            }else{
+                res.send(result);
+            }
+        })
+    }catch{
+        res.status(500).send({message: "Internal server error"});
+    }
+    
 
 })
 
@@ -71,7 +92,7 @@ router.post("/api/students", (req, res)=>{
             }
         })
     }catch(e){
-        res.status(500).send({message: "Internal Server Error"});
+        res.status(500).send({message: "Internal server error"});
     }
     
 })
@@ -82,7 +103,7 @@ router.post("/api/generatePId", (req, res)=>{
     const subject_code = req.body.code;
     const batch_id = req.body.batchId;
     const stamp = req.body.stamp;
-    const pid = instructor_id.concat(subject_code, batch_id, stamp);
+    
     try{
         connection.query(`INSERT INTO period_id (instructor_id, subject_code, batch_id, stamp) VALUES (?, ?, ?, ?)`,
         [instructor_id, subject_code, batch_id, stamp],
@@ -95,7 +116,7 @@ router.post("/api/generatePId", (req, res)=>{
         })
     }catch(e){
         console.log(e);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send({message: "Internal server error"});
     }
     
 })
